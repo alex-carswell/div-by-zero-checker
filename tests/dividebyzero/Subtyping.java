b7adbff83f1ba0880216da3bbdd0ea26aae4df0b
Arturo De la Pena Zaldivar <arturo_pspzaldivar@hotmail.com>
2024-05-27 15:28:09 -0700

    Initial subtyping tests

--- tests/dividebyzero/Subtyping.java
+++ tests/dividebyzero/Subtyping.java
@@ -4,12 +4,20 @@ import org.checkerframework.checker.dividebyzero.qual.*;
 // The file contains "// ::" comments to indicate expected errors and warnings.
 
 class SubtypeTest {
-  /// You will want a test like this for every pair of qualifiers in your type hierarchy.
-  // void oneSubtypingRelationship(@MyTopQualifier int x, @MyBottomQualifier int y) {
-  //   @MyTopQualifier int a = x;
-  //   @MyTopQualifier int b = y;
-  //   // :: error: assignment
-  //   @MyBottomQualifier int c = x; // expected error on this line, as indicated just above
-  //   @MyBottomQualifier int d = y;
-  // }
+  // You will want a test like this for every pair of qualifiers in your type hierarchy.
+  void oneSubtypingRelationship(@Top Integer x, @Bottom Integer y) {
+    @Top Integer a = x;
+    @Top Integer b = y;
+    // :: error: assignment
+    @Bottom Integer c = x; // expected error on this line, as indicated just above
+    @Bottom Integer d = y;
+  }
+
+  void anotherSubtypingRelationship(@Top Integer x, @NegativeOrZero Integer y) {
+    @Top Integer a = x;
+    @Top Integer b = y;
+    // :: error: assignment
+    @NegativeOrZero Integer c = x; // expected error on this line, as indicated just above
+    @NegativeOrZero Integer d = y;
+  }
 }
